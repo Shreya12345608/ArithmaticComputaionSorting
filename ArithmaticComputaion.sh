46c734b7c77998d75b1949667960588bd4a0431d
Shreya Malviya <malviyashreya26@gmail.com>
2021-05-01 13:42:47 +0530

    Add: UC6 stroing the results in dictionary for every computation

--- ArithmaticComputaion.sh
+++ ArithmaticComputaion.sh
@@ -3,7 +3,34 @@ echo "Welocome to Arithmatic Computaion & sorting "
 read -p "Enter value for a:" a
 read -p "enter value for b:" b
 read -p "enter value for c:" c
-echo $(( $a+$b*$c ))
-echo $(( $a*$b+$c ))
-echo $(( $c+$a/$b ))
-echo $(( $a%$b+$c ))
+
+equation1=$(( $a + $b * $c ))
+echo "first equation" $equation1;
+
+
+equation2=$(( $a * $b + $c ))
+echo "second equation" $equation2;
+
+equation3=$(( $c + $a / $b ))
+echo "third equation" $equation3;
+
+equation4=$(( $a % $b + $c ))
+echo "fourth equation" $equation4;
+
+#Storing results in Dictionary
+
+declare -A  dict
+
+dict[1]=$equation1
+dict[2]=$equation2
+dict[3]=$equation3
+dict[4]=$equation4
+
+j=0
+
+echo "Printing Dictonary values";
+
+for(( j=1; j<=4; j++ ))
+do
+echo "Dict[$j]:"${dict[$j]}
+done
